8121b5031a39d940ab90334e69f2d25b7ada5978
Matheus Nunes <devmatheusnunesdebarros@gmail.com>
2023-12-30 16:37:37 -0300

    Adicionado prompt para valor minimo da nota, e colocando duas casas decimais para media

--- main.js
+++ main.js
@@ -3,17 +3,18 @@ const atividade = document.querySelector('#atividade')
 const nota = document.querySelector('#nota')
 let cont = 1
 let media = 0
+let padrao = prompt("Digite a nota mínima: ")
 Adicionar.addEventListener('submit', (evt) => {
     evt.preventDefault()
 
     const aprovado = "<img style='width:32px' src='images/aprovado.png'> </img>"
     const reprovado = "<img style='width:32px' src='images/reprovado.png'> </img>"
-    const condicao = nota.value >= 7 ? aprovado : reprovado
+    const condicao = nota.value >= padrao ? aprovado : reprovado
     const linha = `<tr> <td>${atividade.value}</td> <td>${nota.value}</td> <td>${condicao}</td> </tr>`
 
     media +=  parseFloat(nota.value)
-    let mediaFinal = media/cont >= 7 ? `<td><span class='aprovado'>Aprovado</span></td>` : `<td><span class='reprovado'>Reprovado</span></td>`
-    let linha_media = `<tr> <td>Média</td> <td>${parseFloat(media / cont).toFixed(1)}</td> ${mediaFinal}</tr>`
+    let mediaFinal = media/cont >= padrao ? `<td><span class='aprovado'>Aprovado</span></td>` : `<td><span class='reprovado'>Reprovado</span></td>`
+    let linha_media = `<tr> <td>Média</td> <td>${parseFloat(media / cont).toFixed(2)}</td> ${mediaFinal}</tr>`
     
     document.querySelector('tbody').innerHTML += linha
     document.querySelector('tfoot').innerHTML = linha_media
